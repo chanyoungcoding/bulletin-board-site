e76862a9f6472de1c44560b79ce9a368e6a93e70
chanyoungcoding <white1614@naver.com>
2023-06-07 09:55:18 +0900

    아이디 중복체크

--- ims.php
+++ ims.php
@@ -1,4 +1,18 @@
 <?php
 
-include './inc/dbconfig.php'
+include './inc/dbconfig.php';
+include './inc/member.php';
+
+//아이디 중복 테스트
+
+$id = 'chan';
+
+$mem  = new Member($db);
+
+if ($mem->id_exists($id)) {
+  echo "아이디가 중복됩니다.";
+} else {
+  echo "사용할수 있는 아이디 입니다.";
+}
+
 ?>
---
+++ inc/member.php
@@ -0,0 +1,25 @@
+<!-- 멤버 클래스 파일 -->
+
+<?php 
+
+class Member {
+  //멤버 변수, 프로퍼티
+  private $conn;
+
+  // 생성자
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+
+  //아이디 중복체크용 멤버 함수,메소드
+  public function id_exists($id) {
+    $sql = "SELECT * FROM member WHERE id=:id";  // id= 'chan'; 이렇게 나옴
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    return $stmt->rowCount() ? true : false ;    // 검색했을때 아무것도 없다면 가로열도 0 일 것이다.
+  }
+}
+
+?>
