75f7cb8a103d637aae9a0869b0b544fe29b58eb6
chanyoungcoding <white1614@naver.com>
2023-06-13 14:05:57 +0900

    메인페이지수정

--- inc_header.php
+++ inc_header.php
@@ -9,6 +9,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
+  <!-- font awasome -->
+  <script src="https://kit.fontawesome.com/9c16dfcaa4.js" crossorigin="anonymous"></script>
+
   <!-- swiper  -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
   <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
@@ -34,8 +37,7 @@
     <!-- header -->
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <img src="./img/logo.png"  style="width:2rem" class="me-2"/>
-        <span class="fs-4">동물학교</span>
+        <span class="fs-4"><i class="fa-solid fa-paw"></i>&nbsp;동물학교</span>
       </a>
 
       <ul class="nav nav-pills">
--- index.php
+++ index.php
@@ -11,15 +11,142 @@ $menu_code = '';
 include 'inc_header.php';
 ?>
 <main class="home__main">
-  <div class="swiper-container" id="my-swiper">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide">1</div>
-      <div class="swiper-slide">2</div>
-      <div class="swiper-slide">3</div>
-      <div class="swiper-slide">4</div>
-      <div class="swiper-slide">5</div>
+
+  <!-- 메인 헤더 부분 -->
+  <div class="home__header">
+    <div class="header__preface">
+      <i class="fa-solid fa-bone"></i>
+    </div>
+    
+    <div class="header__animal">
+    <a href="javascript:void(0)"><div>강아지 연구소</div></a>
+    <a href="javascript:void(0)"><div>고양이 연구소</div></a>
     </div>
   </div>
+
+  <!-- 메인 중앙 부분 -->
+  <div class="home__center">
+    <div class="center__informationName">
+      <div><p>인기 지식 정보</p></div>
+    </div>
+
+    <p class="cat">고양이</p>
+    <div class="center__intro">
+      <div class="swiper-container" id="my-swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="dog">강아지</p>
+    <div class="center__intro">
+      <div class="swiper-container" id="my-swiper">
+        <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:void(0)">
+              <div>
+                <img src="./img/example.jpg" alt="이미지 입니다.">
+              </div>
+              <div class="img__intro">
+                가나다라마바사아자차카파타하가나다라마바사
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 </main>
 
 <?php
